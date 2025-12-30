39db8d6467a25938b713e9ede880374abb9ca81b
slandath <jesus_slanda@yahoo.com>
2025-12-30 12:04:05 -0500

    initial commit
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex min-h-screen flex-col text-center bg-gray-300">
+    <header class="bg-red-500 p-4">
+      <h1 class="text-3xl">
+        Pokemon Battle Tool
+      </h1>
+      <p>Find your opponent's pokemon and view the best type match-ups.</p>
+    </header>
+    <main class="flex-1 p-4">
+      <div>
+        <label for="pokemon"> Pokemon Name </label>
+      </div>
+      <div>
+        <input id="pokemon" type="text" class="p-1 border">
+      </div>
+      <article class="bg-yellow-500 p-4 mt-4">
+        <p>Pokemon Name</p>
+        <p>Pokemon Generation</p>
+        <p>Pokemon Type</p>
+        <p>Double Damage</p>
+        <p>Half Damage</p>
+        <p>Zero Damage</p>
+      </article>
+    </main>
+    <footer class="bg-purple-500 p-4">
+      Footer
+    </footer>
+  </div>
+</template>
